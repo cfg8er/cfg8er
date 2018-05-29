5969edc80870ca355b55fff8f521e4d18263667e
Ben Allen <bensallen@me.com>
2018-05-28 22:11:12 -0500

    Implement FileOpenAtRef, remove FileOpen, Rename Clone to CloneBare
    - Renamed Clone to CloneBare, and clone repositories without a working
      tree. A working tree with checkouts of files happening at different refs
      wouldn't be safe or efficient.
    - Implement FileOpenAtRef that returns a io.ReadCloser of the file at a
      given path at a given reference. We'll want to add the functionality
      of accepting a commit hash, or shortcut references like "master".
    - Remove FileOpen since the WorkingTree approach isn't great.

--- repository/repository_test.go
+++ repository/repository_test.go
@@ -4,13 +4,15 @@ import (
 	"bytes"
 	"io/ioutil"
 	"testing"
+
+	"gopkg.in/src-d/go-git.v4/plumbing"
 )
 
 var exampleRepo = "https://github.com/git-fixtures/basic.git"
 
-func TestClone(t *testing.T) {
+func TestCloneBare(t *testing.T) {
 	type args struct {
-		p string
+		URL string
 	}
 	tests := []struct {
 		name    string
@@ -20,31 +22,32 @@ func TestClone(t *testing.T) {
 	}{
 		{
 			name:    "Clone a repo",
-			args:    args{p: exampleRepo},
+			args:    args{URL: exampleRepo},
 			wantErr: false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			_, err := Clone(tt.args.p)
+			_, err := CloneBare(tt.args.URL)
 			if (err != nil) != tt.wantErr {
-				t.Errorf("Clone() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("CloneBare() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
 		})
 	}
 }
 
-func TestRepository_FileOpen(t *testing.T) {
-	r, err := Clone(exampleRepo)
+func TestRepository_FileOpenAtRef(t *testing.T) {
+	r, err := CloneBare(exampleRepo)
 
 	if err != nil {
-		t.Errorf("Repository.FileOpen() error = %v", err)
+		t.Errorf("Repository.FileOpenAtRef() error = %v", err)
 		return
 	}
 
 	type args struct {
-		path string
+		path    string
+		refName plumbing.ReferenceName
 	}
 	tests := []struct {
 		name    string
@@ -53,31 +56,43 @@ func TestRepository_FileOpen(t *testing.T) {
 		wantErr bool
 	}{
 		{
-			name:    "Open and read CHANGELOG",
-			args:    args{path: "CHANGELOG"},
+			name:    "Open and read CHANGELOG at master",
+			args:    args{path: "CHANGELOG", refName: "refs/heads/master"},
 			want:    []byte("Initial changelog\n"),
+			wantErr: false,
+		},
+		{
+			name:    "Open and read CHANGELOG at a remote branch",
+			args:    args{path: "CHANGELOG", refName: "refs/remotes/origin/branch"},
+			want:    []byte("Initial changelog\n"),
+			wantErr: false,
+		},
+		{
+			name: "Open and read vendor/foo.go at master",
+			args: args{path: "vendor/foo.go", refName: "refs/heads/master"},
+			want: []byte("package main\n\nimport \"fmt\"\n\nfunc main() {\n	fmt.Println(\"Hello, playground\")\n}\n"),
 			wantErr: false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-
-			got, err := r.FileOpen(tt.args.path)
+			got, err := r.FileOpenAtRef(tt.args.path, tt.args.refName)
 			if (err != nil) != tt.wantErr {
-				t.Errorf("Repository.FileOpen() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("Repository.FileOpenAtRef() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
 			defer got.Close()
 
 			gotContents, err := ioutil.ReadAll(got)
 			if (err != nil) != tt.wantErr {
-				t.Errorf("Repository.FileOpen() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("Repository.FileOpenAtRef() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
 
 			if !bytes.Equal(gotContents, tt.want) {
-				t.Errorf("Repository.FileOpen() = %s, want %s", gotContents, tt.want)
+				t.Errorf("Repository.FileOpenAtRef() = %v, want %v", gotContents, tt.want)
 			}
+
 		})
 	}
 }
